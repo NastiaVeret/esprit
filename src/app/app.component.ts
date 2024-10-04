cc86b975dc61592cdef520eabad7b37e02ae7c2b
Spoilsport <nastasiyka.ver@gmail.com>
2024-10-04 21:55:05 +0300

    added styles for first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterModule, CommonModule], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'espritapp';
+  showMainPage = false;
+
+  constructor(private router: Router) {}
+
+  navigateToAnotherPage(pageName: string): void {
+    this.router.navigate([`${pageName}`]);
+    this.showMainPage = true; 
+  }
 }
