2a52102aad35bc5acd440741b86b82c5d5caffa2
Maksym Leshchenko <leshchenko.maksym10@gmail.com>
2024-10-05 13:39:48 +0200

    routing fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, CommonModule], 
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'espritapp';
-  showMainPage = false;
-
-  constructor(private router: Router) {}
-
-  navigateToAnotherPage(pageName: string): void {
-    this.router.navigate([`${pageName}`]);
-    this.showMainPage = true; 
-  }
-}
+export class AppComponent {}
