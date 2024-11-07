c2607340d20ca74cf23b23dce3ae4d5d847b90e3
Spoilsport <nastasiyka.ver@gmail.com>
2024-11-07 21:41:51 +0200

    added connection to backend

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,20 +1,39 @@
 import { Component, TemplateRef } from '@angular/core';
-import { Router } from '@angular/router';
-import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';  
+import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClient } from '@angular/common/http';
 import { ModalService } from './modal/services/modal.service';
+
+interface HeartDiseaseResponse {
+  prediction: number[];
+}
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [FormsModule, HttpClientModule],  
   providers: [ModalService],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrls: ['./main-page.component.css']
 })
-
 export class MainPageComponent {
-  constructor(private modalService: ModalService) {}
+  formData: {
+    age: number;
+    sex: boolean;
+    chestPainType: number;
+    restingBP: number;
+    cholesterol: number;
+    fastingBS: number;
+    restingECG: number;
+    maxHR: number;
+    exerciseAngina: boolean;
+    oldpeak: number;
+  } = {} as any;
+
+  constructor(
+    private modalService: ModalService,
+    private http: HttpClient 
+  ) {}
 
   openModal(modalTemplate: TemplateRef<any>) {
     this.modalService
@@ -22,5 +41,32 @@ export class MainPageComponent {
       .subscribe((action) => {
         console.log('modalAction', action);
       });
+  }
+
+  onSubmit(modalTemplate: TemplateRef<any>) {
+    const transformedData = {
+      age: Number(this.formData.age), 
+      sex: Number(this.formData.sex) == 1 ? true : false,
+      chestPainType: Number(this.formData.chestPainType),
+      restingBP: Number(this.formData.restingBP),
+      cholesterol: Number(this.formData.cholesterol),
+      fastingBS: Number(this.formData.fastingBS),
+      restingECG: Number(this.formData.restingECG),
+      maxHR: Number(this.formData.maxHR),
+      exerciseAngina: Number(this.formData.exerciseAngina) == 1 ? true : false, 
+      oldpeak: Number(this.formData.oldpeak)
+    };
+
+    this.http.post<HeartDiseaseResponse>('https://localhost:7133/api/HeartDisease', transformedData)
+      .subscribe(
+        (response) => {
+          if (response.prediction && response.prediction[0] === 0) {
+            this.openModal(modalTemplate); 
+          }
+        },
+        (error) => {
+          console.error('Помилка при відправці:', error);
+        }
+      );
   }
 }
